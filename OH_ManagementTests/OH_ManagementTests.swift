871fcb02f0901d815d0f889c840e23e29f009f1f
Jong Min Choi <jongmin@seas.upenn.edu>
2019-11-22 01:41:36 -0500

    Initial Commit

---
+++ OH_ManagementTests/OH_ManagementTests.swift
@@ -0,0 +1,34 @@
+//
+//  OH_ManagementTests.swift
+//  OH_ManagementTests
+//
+//  Created by CHOI Jongmin on 22/11/2019.
+//  Copyright © 2019 CHOI Jongmin. All rights reserved.
+//
+
+import XCTest
+@testable import OH_Management
+
+class OH_ManagementTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
